abe0313d7eb15a271b4c77f9fc003a8f12869fb7
Max Rustle <csmoke.mk@gmail.com>
2022-06-27 21:57:37 +0300

    Search in posts added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import './styles/App.css'
 import PostsList from './components/PostsList'
 import PostForm from './components/PostForm'
 import MySelect from './components/UI/select/MySelect'
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {id: 1555555555555, title: 'Post header here', body: 'Some text here. Some text here.'},
-    {id: 1444444444444, title: 'Post header here', body: 'Some text here. Some text here.'},
+    {id: 1555555555555, title: 'Post header turtle', body: 'Some text here. Some text here.'},
+    {id: 1444444444444, title: 'Post header here', body: 'Some text here. Some rabbit here.'},
     {id: 1333333333333, title: 'Post header here', body: 'Some text here. Some text here.'},
-    {id: 1222222222222, title: 'Post header here', body: 'Some text here. Some text here.'},
+    {id: 1222222222222, title: 'Post rabbit here', body: 'Some turtle here. Some text here.'},
     {id: 1111111111111, title: 'Post header here', body: 'Some text here. Some text here.'}
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
 
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+
+      if (selectedSort === 'old') {
+        return [...posts].sort((a, b) => a.id - b.id)
+      } else if (selectedSort === 'new') {
+        return [...posts].sort((a, b) => b.id - a.id)
+      }
+    }
+
+    return posts
+
+  }, [selectedSort, posts])
+
+  const sortedSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()) || post.body.toLowerCase().includes(searchQuery.toLowerCase()))
+  }, [searchQuery, sortedPosts])
+
   const createPost = (newPost) => {
     setPosts([newPost, ...posts].sort((a, b) => b.id - a.id))
     setSelectedSort('')
+    setSearchQuery('')
   }
 
   const removePost = (removingPost) => {
@@ -27,18 +49,18 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-
-    if (sort === 'old') {
-      setPosts([...posts].sort((a, b) => a.id - b.id))
-    } else if (sort === 'new') {
-      setPosts([...posts].sort((a, b) => b.id - a.id))
-    }
   }
 
   return (
     <div className='container'>
 
       <PostForm create={createPost}/>
+
+      <MyInput
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+        placeholder='Search post...'
+      />
 
       <MySelect
         value={selectedSort}
@@ -50,8 +72,8 @@ function App() {
         ]}
       />
 
-      {posts.length
-        ? <PostsList posts={posts} remove={removePost}/>
+      {sortedSearchedPosts.length
+        ? <PostsList posts={sortedSearchedPosts} remove={removePost}/>
         : <h1>No posts found...</h1>
       }
 
